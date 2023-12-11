86a7b70823b4c084af80fd04dd7c82cc4d714fad
utsav <guptautsavkumar126@gmail.com>
2023-12-12 00:13:21 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  let[val,setVal]=useState("");
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Enter your name: </p>
+        <input type="text" onInput={(event)=>setVal(event.target.value)}/>
+        {val ? <p>Hello {val}!</p>:""}
     </div>
   )
 }
